6f9a379de5b5248234a1228e62196db23605d336
Nivaldo Melo <nivaldogmelo@gmail.com>
2024-05-20 23:04:32 -0300

    feat(auth): Update lib and main to use AppState

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,10 +1,14 @@
+use app_state::AppState;
 use axum::{routing::post, serve::Serve, Router};
-use std::error::Error;
+use std::{error::Error, sync::Arc};
 use tower_http::services::ServeDir;
 
 use routes::*;
 
+pub mod app_state;
+pub mod domain;
 pub mod routes;
+pub mod services;
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -13,14 +17,15 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn build(app_state: Arc<AppState>, address: &str) -> Result<Self, Box<dyn Error>> {
 	let router = Router::new()
 	    .nest_service("/", ServeDir::new("assets"))
 	    .route("/signup", post(signup))
 	    .route("/login", post(login))
 	    .route("/verify-2fa", post(verify_2fa))
 	    .route("/logout", post(logout))
-	    .route("/verify-token", post(verify_token));
+	    .route("/verify-token", post(verify_token))
+	    .with_state(app_state);
 
 	let listener = tokio::net::TcpListener::bind(address).await?;
 	let address = listener.local_addr()?.to_string();
