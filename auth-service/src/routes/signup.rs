ffb8cb98167ad86fb076ee3a191a4f5417fe0f35
Nivaldo Melo <nivaldogmelo@gmail.com>
2024-05-20 23:05:00 -0300

    feat(auth): Implement sign-up route success case

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,8 +1,27 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
-use serde::Deserialize;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 
-pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
-    StatusCode::CREATED.into_response()
+use crate::{app_state::AppState, domain::User};
+
+pub async fn signup(
+    State(state): State<Arc<AppState>>,
+    Json(request): Json<SignupRequest>,
+) -> impl IntoResponse {
+    let user = request.into_user();
+    let mut user_store = state.user_store.write().await;
+    let _ = user_store.add_user(user);
+
+    let response = Json(SignupResponse {
+	message: "User created successfully!".to_string(),
+    });
+
+    (StatusCode::CREATED, response)
+}
+
+#[derive(Serialize)]
+pub struct SignupResponse {
+    pub message: String,
 }
 
 #[derive(Deserialize)]
@@ -11,4 +30,10 @@ pub struct SignupRequest {
     pub password: String,
     #[serde(rename = "requires2FA")]
     pub requires_2fa: bool,
+}
+
+impl SignupRequest {
+    fn into_user(&self) -> User {
+	User::new(self.email.clone(), self.password.clone(), self.requires_2fa)
+    }
 }
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,6 +1,28 @@
 use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
+async fn should_return_201_if_valid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [
+	serde_json::json!({"email": random_email, "password": "password123", "requires2FA": true}),
+    ];
+
+    for test_case in test_cases.iter() {
+	let response = app.post_signup(test_case).await;
+
+	assert_eq!(
+	    response.status().as_u16(),
+	    201,
+	    "Test case failed: {:?}",
+	    test_case
+	);
+    }
+}
+
+#[tokio::test]
 async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
